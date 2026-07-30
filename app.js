40ba2a986df8fd2e2822573913a7aaa925573268
Cristian <martinezduartecristiandavid24@gmail.com>
2026-07-30 13:56:20 -0500

    pruebas con get

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { request } from 'express';
 import 'dotenv/config';
 
 const app = express();
@@ -7,6 +7,16 @@ const port = process.env.PORT || 3000;
 app.get("/", (_, res) => {
  res.send('Hola , estamos aprendiendo express con la ficha 3407184');});
 
-app.listen(port, () => {
+//prueba uso de get simple
+ app.get("/Ping", (req, res) => {
+    res.send("Pong")})
+
+//prueba uso de get personalizado
+app.get("/saludo/:nombre", (req, res) => {
+    const nombre = req.params.nombre
+    res.send(`Hola, ${nombre}, bienvenido a mi prueba en express`);  //siempre se usan `` (alt gr + } )
+})
+ app.listen(port, () => {
  console.log( `Servidor en funcionamiento en el puerto: ${port}`);
 });
+
