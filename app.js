e8eae7f788a93d9263944e3224b438463fbdba0d
Cristian <martinezduartecristiandavid24@gmail.com>
2026-08-06 16:31:31 -0500

    Prueba commonjs y volver a commits anteriores

--- app.js
+++ app.js
@@ -2,9 +2,26 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
 app.get("/", (_, res) => {
  res.send('Hola , estamos aprendiendo express con la ficha 3407184');});
 
+
+app.get("/datos", (req, res) => {
+    const personales = {
+        mensaje : "Datos personales",
+        nombre : "Cristian",
+        apellido : "Martinez",
+        telefono : "3115449740",
+    }
+    const programa = {        
+        mensaje : "Datos de programa",
+        nombre : "ADSO",
+        tipo : "tecnologo ",
+        ficha : "3407184",
+    }   
+res.json([personales, programa])
+})
 app.listen(port, () => {
  console.log( `Servidor en funcionamiento en el puerto: ${port}`);
 });
