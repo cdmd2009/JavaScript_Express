2a91e95f41bd7240a570cdfcfd5fb7fcf0d83586
Cristian <martinezduartecristiandavid24@gmail.com>
2026-08-13 13:33:28 -0500

    app inicial con commonjs y uso de function

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/", (_, res) => {
+app.get("/", function(req, res) {
  res.send('Hola , estamos aprendiendo express con la ficha 3407184');});
 
-app.listen(port, () => {
- console.log( `Servidor en funcionamiento en el puerto: ${port}`);
+app.listen(port, function() {
+ console.log( `Servidor: http://localhost:${port}`);
 });
